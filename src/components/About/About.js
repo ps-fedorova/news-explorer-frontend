685bd5e95d9e26bbf479a094c64eea1f1f2036ab
Polina Fedorova <ps.fedorova@yandex.ru>
2020-10-25 22:08:41 +0500

    feat: add adaptive to the section About

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,16 +6,17 @@ function About() {
   return (
     <section className="about">
       <div className="about__container">
-        <img src={author} alt="Фото автора"/>
-        <div className="author__container">
-          <h3 className="author__title">Об авторе</h3>
-          <p className="author__text">
-            Это блок с описанием автора проекта. Здесь следует указать, как вас зовут, чем вы занимаетесь, какими
-            технологиями разработки владеете.
+        <img className="author-img" src={author} alt="Фото автора"/>
+        <div className="description">
+          <h2 className="description__title">Об авторе</h2>
+          <p className="description__text">
+            Это блок с описанием автора проекта.
+            Здесь следует указать, как вас зовут, чем вы занимаетесь,
+            какими технологиями разработки владеете.
           </p>
-          <p className="author__text">
-            Также можно рассказать о процессе обучения в Практикуме, чему вы тут научились, и чем можете помочь
-            потенциальным заказчикам.
+          <p className="description__text">
+            Также можно рассказать о процессе обучения в Практикуме,
+            чему вы тут научились, и чем можете помочь потенциальным заказчикам.
           </p>
         </div>
       </div>
