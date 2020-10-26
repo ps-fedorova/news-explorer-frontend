5365c96a8583233b0a289c4049831d09df06139b
Polina Fedorova <ps.fedorova@yandex.ru>
2020-10-27 00:57:17 +0500

    fix: style

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -6,7 +6,7 @@ function Button(props) {
     <button className={`button button_outline
     ${props.header ? 'header__button' : ''}
     ${props.menuMobile ? 'menu-mobile__button' : ''}
-
+    ${props.searchForm ? 'search-form__button' : ''}
     `}>
 
       {props.header
@@ -17,6 +17,7 @@ function Button(props) {
           </div>
           <img className={props.classNameImg} src={props.src} alt={props.alt}/>
         </div>
+
         : <div className="button__container">
           <div className="button__text" title={props.value}>
             {props.value}
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import './SearchForm.css';
+import Button from '../Button/Button';
 
 function SearchForm() {
   return (
     <section className="search-form">
       <div className="search-form__container">
-        {/* <h1 className="search-form__title" >Что творится в мире?</h1> */}
+        <h1 className="search-form__title">Что творится в мире?</h1>
+        <p className="search-form__subtitle">
+          Находите самые свежие статьи на любую тему и сохраняйте в своём личном
+          кабинете.
+        </p>
+
+        <div className="search-form__input-container">
+          <input
+            title="Введите запрос"
+            type="text"
+            className="search-form__input"
+            placeholder="Введите тему овости"
+          />
+          <Button
+            searchForm={true}
+            value="Искать"
+            className="search-form__button"
+          />
+        </div>
       </div>
     </section>
   );
