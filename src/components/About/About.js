d9f6d73b5c0f8721dad167ed788f66131e57677b
Polina Fedorova <ps.fedorova@yandex.ru>
2020-10-29 15:34:01 +0500

    fix: add tablet for About

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,15 +6,15 @@ function About() {
   return (
     <section className="about">
       <div className="about__container">
-        <img className="author-img" src={author} alt="Фото автора"/>
-        <div className="description">
-          <h2 className="description__title">Об авторе</h2>
-          <p className="description__text">
+        <img className="about__author-img" src={author} alt="Фото автора"/>
+        <div className="about__container-description">
+          <h2 className="about__title-description">Об авторе</h2>
+          <p className="about__text-description">
             Это блок с описанием автора проекта.
             Здесь следует указать, как вас зовут, чем вы занимаетесь,
             какими технологиями разработки владеете.
           </p>
-          <p className="description__text">
+          <p className="about__text-description">
             Также можно рассказать о процессе обучения в Практикуме,
             чему вы тут научились, и чем можете помочь потенциальным заказчикам.
           </p>
